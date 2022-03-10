b534580296befd8cb0d71d404bd812b7cbdba1db
vickyruud <aprajit.k@gmail.com>
2022-03-10 08:50:09 -0500

    add keyboard component and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { wordList } from './constants/data';
+
 
 const App = () => {
+
+  //use state for board data which stores the data from the board to save the ongoing game
   const [boardData, setBoardData] = useState(JSON.parse(localStorage.getItem('board-data')));
+  //store the array of characters in state
+  const [charArray, setCharArray] = useState([]);
+
+  useEffect(() => {
+    if (!boardData || !boardData.solution) {
+      let alphabetIndex = Math.floor(Math.random() * 26);
+      let wordIndex = Math.floor(Math.random() * wordList[String.fromCharCode(97 + alphabetIndex)].length);
+      let newBoardData = {
+        ...boardData, "solution": wordList[String.fromCharCode(97 + alphabetIndex)][wordIndex],
+        "rowIndex": 0,
+        "boardWords": [],
+        "boardRowStatus": [],
+        "presentCharArray": [],
+        "absentCharArray": [],
+        "correctCharArray": [],
+        "status": "IN_PROGRESS"
+      };
+      setBoardData(newBoardData);
+      localStorage.setItem("board-data", JSON.stringify(newBoardData));
+   }
+  }, [])
   
+
 
   return (
     <div>
---
+++ src/component/Keyboard.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { keys } from '../constants/constants'
+import '../styles/Keyboard.css'
+
+const Keyboard = ({boardData, handleKeyPress}) => {
+  return (
+    <div className='keyboard-rows'>
+      {keys.map((item, i) => (
+        <div className='row' key={index}>
+          {
+            item.map((key, keyIndex) => (
+              <button 
+                key={keyIndex}
+                className={`${boardData && boardData.correctCharArray.includes(key)? "key-correct" :
+                    (boardData && boardData.presentCharArray.includes(key) ? "key-present" : 
+                    boardData && boardData.absentCharArray.includes(key) ? "key-absent" : " ")}`}
+                onClick = {() => {handleKeyPress(key)}}
+              >
+                {key}
+
+              </button>
+            ))
+        }
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Keyboard
